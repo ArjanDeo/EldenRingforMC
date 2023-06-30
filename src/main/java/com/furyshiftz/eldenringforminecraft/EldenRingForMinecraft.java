33d6cc7d4259109a5250a7fcccba99364b2a211b
ArjanDeo <arjandeo@bluecomet-it.com>
2023-06-30 14:17:22 +0100

    Inr

---
+++ src/main/java/com/furyshiftz/eldenringforminecraft/EldenRingForMinecraft.java
@@ -0,0 +1,7 @@
+package com.furyshiftz.eldenringforminecraft;
+
+import net.minecraftforge.fml.common.Mod;
+@Mod(EldenRingForMinecraft.MODID)
+public class EldenRingForMinecraft {
+    public static final String MODID = "erfmc";
+}
